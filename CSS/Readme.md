b4f1e586899a39224e194f24ddbc9bca62b48c1d
terraformmedia <31185372+terraformmedia@users.noreply.github.com>
2020-07-23 19:48:52 +0100

    Added movie covers and new divider

--- CSS/Readme.md
+++ CSS/Readme.md
@@ -1,2 +1,6 @@
 ### Image Sources
 - Book covers - https://i.pinimg.com/originals/90/01/50/9001500e8e8e216ea325261dec334c9c.png
+
+- Movie covers - https://www.wikipedia.org 
+
+- Divider - https://codepen.io/alexandr-kazakov/pen/QOOwQo
--- JS/main.js
+++
@@ -1,9 +0,0 @@
-const ul = document.getElementById('content');
-const url = 'https://the-one-api.herokuapp.com/v1/book';
-
-fetch(url)
-    .then((resp) => resp.json()) // Transform the data into json
-    .then(function (data) {
-        // Create and append the li's to the ul
-        console.log(data);
-    })
